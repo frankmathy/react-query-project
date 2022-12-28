dd527dbbf9964d2d950e6ec1268444ad38854a94
Frank Mathy <frankmathy@gmail.com>
2022-12-28 17:27:48 +0100

    React Query version implemented

--- react-query-project/src/components/RQSuperHeroes.page.js
+++ react-query-project/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,20 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
 export const RQSuperHeroesPage = () => {
-  return <div>RQ Super Heroes Page</div>;
+  const { isLoading, data } = useQuery('super-heroes', () => {
+    return axios.get('http://localhost:4000/superheroes');
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
